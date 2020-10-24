4e160a642ecc4d9aa14c04fc0caecc524f10fdd0
Archivist Nerd <72463375+Archivist-Nerd@users.noreply.github.com>
2020-10-24 16:46:38 -0400

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # node-args
-simple process.argv processor
+[![npm version](https://img.shields.io/npm/v/@archivistnerd/args.svg)](https://www.npmjs.com/package/@archivistnerd/args)
+
+Archivist's Args : Simple process.argv Parser
+
+> Small, simplistic process.argv parser for node cli apps.
+
+## Installation
+
+```sh
+npm install @archivistnerd/args
+```
+
+## Usage
+
+```javascript
+const args = require('@archivistnerd/args')
+
+console.log( args.argv )
+```
+
+
+## License
+
+MIT
---
+++ args.js
@@ -0,0 +1,65 @@
+/*
+        Copyright (c) 2020 Archivist-Nerd
+*/
+'use strict';
+
+const regex = {
+                flags :  /^(-)([^-\s\d\.][^\s\d=]*)=?/i,
+                option:  /^(--)([^-\s\d\.][^\s=]*)=?/i,
+                string:   /^"([^"\\]*(\\.[^"\\]*)*)"/i,
+                number:   /^([\+-]?(\d*\.?)\d+)$/i,
+                values:   /^(\S+)/i,
+              }
+    , $0 = (process.argv[1].indexOf( process.cwd() )=== 0)? process.argv[1].substr(process.cwd().length+1):process.argv[1]
+    ;
+
+module.exports = ( ( argv=process.argv.slice(2) )=>{
+  /*
+
+          process function
+
+  */
+  function process( _argv=[], args=(Array.isArray(_argv))? _argv.join(' '):_argv ){
+    const setValue = ( tag, value=true )=>values[tag]=(values[tag]==undefined)? value
+                                                                             : (!Array.isArray(values[tag]))? [ values[tag], value]
+                                                                                                            : [ ...values[tag], value ]
+        , getValue = ( match=args.match( regex.string ) )=>{
+                            if (isTag()) return true
+                            if (match==null) match=args.match( regex.values )
+                            args = args.substr(match[0].length).trim()
+                            return regex.number.test( match[1] )? parseFloat(match[1]):match[1]
+                          }
+        , isTag    = ()=>(args.match( regex.option ) || args.match( regex.flags ))
+        ;
+    let values  = {
+                    _: []
+                  }
+      , match
+      ;
+    while (args.length){
+      args = args.trim()
+      if ( match=isTag() ){     // flag or option
+        args = args.substr(match[0].length).trim()
+        if (match[1].length==1){          // flags
+          let flags = match[2].split('')
+            , last  = flags.pop()
+          flags.forEach( flag=>{
+            values[flag]=(values[flag])? [...values[flag], true]:[true]
+          })
+          setValue( last, getValue() )
+        } else                            // option
+          setValue( match[2], getValue() );
+      } else                              // value
+        values._.push( getValue() );
+    }
+    values.$0 = $0
+    return values
+  }
+  /*
+      Object to Return
+  */
+  return {
+    argv: process( argv ),
+    process,
+  }
+})();
